0bb93d71163a73d655178e2a97fee08ec0a906ca
Alvin <alvinl70@nycstudents.net>
2022-10-18 00:44:17 -0400

    ok

---
+++ src/LuckyNumbersRunner.java
@@ -0,0 +1,7 @@
+public class LuckyNumbersRunner {
+    public static void main(String[] args) {
+        LuckyNumbers luckyNum = new LuckyNumbers();
+        String str = luckyNum.getLuckyNumbers();
+        System.out.println(str);
+    }
+}
---
+++ src/MathClass.java
@@ -0,0 +1,9 @@
+public class MathClass {
+    public static void main(String[] args) {
+        double cos = Math.cos(5.5);
+        System.out.println(cos);
+
+        double nextDown = Math.nextDown(4.7);
+        System.out.println(nextDown);
+    }
+}
